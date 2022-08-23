f00d35ab614bdc35f093f512a04b169c622f29a7
Manas Sharma <manaskgp2593@kgpian.iitkgp.ac.in>
2022-08-23 23:11:43 +0530

    Fourth commit

--- apicall.js
+++ apicall.js
@@ -129,3 +129,63 @@ $('#lsmForm').submit((e)=>{
 				}
 			});
 })
+
+
+// Query form
+$('#contactForm').submit((e)=>{
+	e.preventDefault();
+	console.log("contactform");
+			   var name = $('#name_c').val();
+			  var email = $('#email_c').val();
+			  var subject = $('#subject_c').val();
+			  var message = $('#message_c').val();
+			//   var startup = $('#startup').val();
+			//   var stage = $('#stage').val();
+			//   var startup_name = $('#startup_name').val();
+			//   var fundr = $('#fundr').val();
+  // 			var loan = $('#loan').val();
+  // 			var loan_type = $('#loantype').val();
+			//   var description = $('#description').val();
+			//   var referral_code=$('#referralcode').val();
+			  // var events = $('.events').val();
+// 			   var events ="";
+// 			  $.each($("input[name='events']:checked"), function(){            
+// 				  events+=($(this).val());
+// 			  });
+// 			  var lfunds = $('#lfunds').val();
+//   // 			var pitchdeck = $('#pitchdeck').val();
+// 			  var sector = $('#sector').val();
+			$.ajax({
+				  url: 'https://api.ecell-iitkgp.org/forms/contactead/',
+				  method: 'POST',
+				  data: {
+					  name: name,
+					  email: email,
+					  subject: subject, 
+					  message: message, 
+					//   startup: startup, 
+					//   stage: stage, 
+					//   startup_name: startup_name, 
+					//   fundr: fundr, 
+  // 					loan: loan,
+  // 					loan_type: loan_type,
+					//   description: description, 
+					//   events: events, 
+					//   lfunds: lfunds, 
+  // 					pitchdeck: pitchdeck,
+					//   sector: sector,
+					//   referral_code:referral_code,
+					  
+				  },
+				  success: function (data) {
+		 swal('','sucess in submition contact form','success');
+			document.getElementById('contactForm').reset();
+				  },
+				  cache: false,
+				  error: function () {
+			  swal('','failed to submit contact from',"error")
+				  
+  
+				  }
+			  });
+  })
